1442db48d1fd532b1e80adb542859d312b1ee28d
Tóth Barnabás <toth.barnabas0314@gmail.com>
2024-09-24 18:41:17 +0200

    Szorgalmi 2

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Page } from "../../components/page/Page";
+
+const NotFoundPage = () => {
+  return (
+    <Page title="404">
+      <h1 className="text-center">Page Not Found 😢</h1>
+    </Page>
+  );
+};
+
+export default NotFoundPage;
